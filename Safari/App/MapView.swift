fffc17d6063bc03268976df7a26d328413c53918
Shubham Kumar <kumar.shubham1329@gmail.com>
2022-05-10 19:20:31 +0530

    Map with markers added

--- Safari/App/MapView.swift
+++ Safari/App/MapView.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
     var body: some View {
-        Text("Map")
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+            //            MapMarker(coordinate: item.location, tint: .accentColor)
+            // Custom
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 32)
+            }
+        }
     }
 }
 
